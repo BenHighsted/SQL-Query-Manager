1adb20097aa5a6a7418d46e7a9ac21d02a76a922
BuildTools <unconfigured@null.spigotmc.org>
2021-07-17 00:17:22 +1200

    Basic styling testing

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,15 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+
 public class Controller {
+    @FXML private Text actionTarget;
+
+    @FXML protected void handleSubmitButtonAction(ActionEvent event) {
+        actionTarget.setText("Sign in button pressed");
+        actionTarget.setFill(Color.RED);
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,17 +5,22 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
 
 public class Main extends Application {
+    Button button;
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(new Scene(root, 300, 275));
+
+        primaryStage.setTitle("Query Manager");
+
+        Scene scene = new Scene(root, 800, 600);
+        primaryStage.setScene(scene);
+
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
